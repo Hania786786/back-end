8ee7a312c41a94ef33a40757223b458d9c01142c
Hania786786 <124409100+Hania786786@users.noreply.github.com>
2023-10-11 18:18:29 +0500

    Update Order.js

--- controllers/Order.js
+++ controllers/Order.js
@@ -15,8 +15,8 @@ const createOrder = async (req, res) => {
 // fetch orders of user
 const getOrdersByUserId = async (req, res) => {
     try {
-        const {user} = req.query;
-        const order = await Order.find({user});
+        const {userId} = req.params;
+        const order = await Order.find({user: userId});
         res.status(200).json(order);
     } catch (error) {
         res.status(400).json(`Error in fetching orders: ${error}`);
@@ -44,6 +44,38 @@ const deleteOrder = async (req, res) => {
         res.status(400).json(`Error in deleting order: ${error}`);
     }
 }
+
+// fetchAllOrders for admin 
+const fetchAllOrders = async (req, res) => {
+    // TODO: response 200 but displaying null
+   let query = Order.find({deleted:{$ne:true}});
+   let totalOrdersQuery = Order.find({deleted:{$ne:true}});
+  
+   //  // for sorting filter
+    if(req.query._sort && req.query._order){
+     // TODO: Sorting through discounted price
+     query = query.sort({[req.query._sort]:req.query._order});
+     totalOrdersQuery = totalOrdersQuery.sort({[req.query._sort]:req.query._order});
+   }
+  
+   // // pagination filter
+    const totalDocs = await totalOrdersQuery.count().exec(); // no. of documents
+   if(req.query._page && req.query._limit){
+     const pageSize = req.query._limit;
+     const page = req.query._page;
+     query = query.skip(pageSize * (page - 1)).limit(pageSize);
+     totalProductsQuery = totalProductsQuery.skip(pageSize * (page - 1)).limit(pageSize);
+   }
+  
+   try {
+     const product = await query.exec();
+     res.set("X-Total-Count", totalDocs); 
+     res.status(200).json(product);
+   } catch (e) {
+     res.status(400).json(e);
+   }
+  };
+
 
 module.exports = createOrder;
 module.exports = getOrdersByUserId;
