726351c038cb8a6c7f541af279fef72f46e8d87a
malikarslanasif131 <109231105+malikarslanasif131@users.noreply.github.com>
2023-10-12 17:38:27 +0500

    Update conn.js

--- config/conn.js
+++ config/conn.js
@@ -1,9 +1,20 @@
-const  mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const colors = require("colors");
+
+dotenv.config();
 const connectDB = async () => {
   try {
-    const connectionDB = await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+    // const connectionDB = await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+    // ADD your Own MONGODB Connection     //Malik Arslan Asif
+    const connectionDB = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    // console.log("Connected to Mongodb database");
     console.log(
-      "Connected to Mongodb database");
+      `MongoDB Connected: ${connectionDB.connection.host}`.bgGreen.white
+    );
   } catch (error) {
     console.log(`Error in Mongoose connection: ${error}`);
   }
