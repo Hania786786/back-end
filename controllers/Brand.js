8f93fb99ce4e7455904c132d3d07dbaadb23900d
malikarslanasif131 <109231105+malikarslanasif131@users.noreply.github.com>
2023-10-12 20:47:38 +0500

    Update Brand.js

--- controllers/Brand.js
+++ controllers/Brand.js
@@ -3,9 +3,10 @@ const Brand = require("../models/Brand");
 // create brand  -- create -- post
 const createBrand = async (req, res) => {
   try {
-    const brand = new Brand(req.body); 
+    const { label, value } = req.body;
+    const brand = new Brand({ label, value });
     const doc = await brand.save();
-    res.status(201).send(doc);  
+    res.status(201).send(doc);
   } catch (e) {
     res.status(400).send(e);
   }
@@ -13,15 +14,16 @@ const createBrand = async (req, res) => {
 
 // fetch all brands -- get -- read
 const fetchBrands = async (req, res) => {
-    try {
-        const brands = await Brand.find({}).exec();
-        res.status(200).json(brands);
-    } catch (e) {
-        res.status(400).json(e);
-    }
+  try {
+    console.log("Malik");
+    const brands = await Brand.find({}).exec();
+    res.status(200).json(brands);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 };
 
-
-
-module.exports = fetchBrands;
-module.exports = createBrand; 
+module.exports = {
+  fetchBrands,
+  createBrand,
+};
