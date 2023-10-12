a38191e499853b43a958ceb06c133d4382e3d221
malikarslanasif131 <109231105+malikarslanasif131@users.noreply.github.com>
2023-10-12 20:54:27 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,24 +2,38 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true },
-  role: {type: String, required: true, default:"user"},
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "user",
+  },
   addresses: { type: [String] },
-  // TODO: We can make separate schema for this   
-  name:{type: String},
-  orders:{type: [String]} 
+  // TODO: We can make separate schema for this
+  name: { type: String },
+  orders: { type: [String] },
 });
 
 // Turning _id to id
 const virtual = userSchema.virtual("id");
-virtual.get(()=>{
+virtual.get(() => {
   return this._id;
 });
 userSchema.set("toJSON", {
   virtuals: true,
   versionKey: false,
-  transform: (doc, ret) => {delete ret._id}
+  transform: (doc, ret) => {
+    delete ret._id;
+  },
 });
 
 const User = mongoose.model("User", userSchema);
