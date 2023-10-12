f2d8fb305185fe125e934a8252e20d22dca31ed8
malikarslanasif131 <109231105+malikarslanasif131@users.noreply.github.com>
2023-10-12 20:48:59 +0500

    Update Product.js

--- controllers/Product.js
+++ controllers/Product.js
@@ -10,58 +10,62 @@ const createProduct = async (req, res) => {
   try {
     const product = new Product(req.body); // get data from body or used to post data
     const createProduct = await product.save();
-    res.status(201).json(createProduct);   // status(201) to post data
+    res.status(201).json(createProduct); // status(201) to post data
   } catch (e) {
     res.status(400).send(e);
   }
 };
 
 //            User base
-// fetch all products -- get -- read 
+// fetch all products -- get -- read
 const fetchAllProducts = async (req, res) => {
-   // TODO: response 200 but displaying null
+  // TODO: response 200 but displaying null
 
   let condition = {};
-  if(!req.query.admin){
+  if (!req.query.admin) {
     condition.deleted = true;
   }
   let query = Product.find(condition);
   let totalProductsQuery = Product.find(condition);
 
   // category filter
-  if(req.query.category){
-    query = query.find({category:req.query.category});
-    totalProductsQuery = totalProductsQuery.find({category:req.query.category});
+  if (req.query.category) {
+    query = query.find({ category: req.query.category });
+    totalProductsQuery = totalProductsQuery.find({
+      category: req.query.category,
+    });
   }
 
   // // brand filter
-  if(req.query.brand){
-    query =  query.find({brand:req.query.brand});
-    totalProductsQuery = totalProductsQuery.find({brand:req.query.brand});
+  if (req.query.brand) {
+    query = query.find({ brand: req.query.brand });
+    totalProductsQuery = totalProductsQuery.find({ brand: req.query.brand });
   }
 
   //  // for sorting filter
-   if(req.query._sort && req.query._order){
+  if (req.query._sort && req.query._order) {
     // TODO: Sorting through discounted price
-    query = query.sort({[req.query._sort]:req.query._order});
-    totalProductsQuery = totalProductsQuery.sort({[req.query._sort]:req.query._order});
+    query = query.sort({ [req.query._sort]: req.query._order });
+    totalProductsQuery = totalProductsQuery.sort({
+      [req.query._sort]: req.query._order,
+    });
   }
 
   // // pagination filter
-   const totalDocs = await totalProductsQuery.count().exec(); // no. of documents
-  if(req.query._page && req.query._limit){
+  const totalDocs = await totalProductsQuery.count().exec(); // no. of documents
+  if (req.query._page && req.query._limit) {
     const pageSize = req.query._limit;
     const page = req.query._page;
     query = query.skip(pageSize * (page - 1)).limit(pageSize);
-    totalProductsQuery = totalProductsQuery.skip(pageSize * (page - 1)).limit(pageSize);
+    totalProductsQuery = totalProductsQuery
+      .skip(pageSize * (page - 1))
+      .limit(pageSize);
   }
-
- 
 
   try {
     // we need all query strings
     const product = await query.exec();
-    res.set("X-Total-Count", totalDocs); // set header 
+    res.set("X-Total-Count", totalDocs); // set header
     res.status(200).json(product);
   } catch (e) {
     res.status(400).json(e);
@@ -70,7 +74,7 @@ const fetchAllProducts = async (req, res) => {
 
 // fetch product by id -- get -- read
 const fetchProductById = async (req, res) => {
-  const {id}= req.params;
+  const { id } = req.params;
   try {
     const product = await Product.findById(id).exec();
     res.status(200).json(product);
@@ -82,16 +86,19 @@ const fetchProductById = async (req, res) => {
 // update product -- update -- PATCH
 const updateProduct = async (req, res) => {
   try {
-    const {id} = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body, {new: true});
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
     res.status(200).json(product);
   } catch (e) {
     res.status(400).json(e);
   }
 };
 
-module.exports = createProduct;
-module.exports = fetchAllProducts;
-module.exports = fetchProductById;
-module.exports = updateProduct;
-
+module.exports = {
+  createProduct,
+  fetchAllProducts,
+  fetchProductById,
+  updateProduct,
+};
