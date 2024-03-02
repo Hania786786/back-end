89e993fdd62f306698a9c402cec57fb50e407031
Shahbaz Khalid <130798658+ShahbazKhaliddev@users.noreply.github.com>
2024-03-02 11:44:53 +0500

    Update Category.js

--- controllers/Category.js
+++ controllers/Category.js
@@ -1,4 +1,4 @@
-const Category = require("../models/Category");
+const Category = require("../models/Category")
 
 // create category  -- create -- post
 const createCategory = async (req, res) => {
