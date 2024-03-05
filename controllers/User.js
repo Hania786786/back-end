a7014915a08816b25298626d5342e271de60ee1f
Shahbaz Khalid <130798658+ShahbazKhaliddev@users.noreply.github.com>
2024-03-05 17:52:26 +0500

    Update User.js

--- controllers/User.js
+++ controllers/User.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 
-// fetch all categories -- get -- read
+// fetch all categories -- get read
 const fetchUserById = async (req, res) => {
   const { id } = req.params;
   try {
