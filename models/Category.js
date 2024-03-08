f280f6ebd1462a0392570f5023f20c3d45950a46
Shahbaz Khalid <130798658+ShahbazKhaliddev@users.noreply.github.com>
2024-03-08 10:50:28 +0500

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 const { Schema } = mongoose;
+
 const categorySchema = new mongoose.Schema({
   label: {
     type: String,
