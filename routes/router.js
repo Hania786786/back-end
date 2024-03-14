4b327e27c69fe75173c612f196c3e3c051940535
Shahbaz Khalid <130798658+ShahbazKhaliddev@users.noreply.github.com>
2024-03-14 16:30:57 +0500

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -4,8 +4,8 @@ const router = express.Router();
 // importing controllers
 const productController = require("../controllers/Product");
 // const createProduct = require("../controllers/Product");
-// const fetchAllProducts = require("../controllers/Product");
-// const fetchProductById = require("../controllers/Product");
+const fetchAllProducts = require("../controllers/Product");
+const fetchProductById = require("../controllers/Product");
 const updateProduct = require("../controllers/Product");
 const brandController = require("../controllers/Brand");
 const createBrand = require("../controllers/Brand");
