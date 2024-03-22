0b06bce90c18ab23c8ed4b5da29f195dfd818498
Shahbaz Khalid <130798658+ShahbazKhaliddev@users.noreply.github.com>
2024-03-22 22:04:57 +0500

    Update Product.js

--- routes/Product.js
+++ routes/Product.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+// importing controllers
+const productController = require("../controllers/Product");
+const createProduct = require("../controllers/Product");
+const fetchAllProducts = require("../controllers/Product");
+const fetchProductById = require("../controllers/Product");
+const updateProduct = require("../controllers/Product");
 
 
 
