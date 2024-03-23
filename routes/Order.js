86c7e20f6aa3be818c5a4b2049703885da3727a0
Shahbaz Khalid <130798658+ShahbazKhaliddev@users.noreply.github.com>
2024-03-23 20:36:09 +0500

    Update Order.js

--- routes/Order.js
+++ routes/Order.js
@@ -6,3 +6,12 @@ const getOrdersByUserId = require("../controllers/Order");
 const updateOrder = require("../controllers/Order");
 const deleteOrder = require("../controllers/Order");
 const fetchAllOrders = require("../controllers/Order");
+
+
+/* ==================
+             order routes
+             ======================*/
+router.post("/orders", createOrder);
+router.get("/orders", getOrdersByUserId);
+router.get("/orders/:id", deleteOrder);
+router.patch("/orders/:id", updateOrder);
