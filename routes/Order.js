bb90c9eb16432d79f85f6bc54a830cd16fac9abf
Shahbaz Khalid <130798658+ShahbazKhaliddev@users.noreply.github.com>
2024-03-23 20:35:25 +0500

    Update Order.js

--- routes/Order.js
+++ routes/Order.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const router = express.Router();
+
+const orderController = require("../controllers/Order");
+const getOrdersByUserId = require("../controllers/Order");
+const updateOrder = require("../controllers/Order");
+const deleteOrder = require("../controllers/Order");
+const fetchAllOrders = require("../controllers/Order");
