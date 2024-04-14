adc94431821c400fc1a489217506f1ae6a4620fa
Shahbaz Khalid <130798658+ShahbazKhaliddev@users.noreply.github.com>
2024-04-14 13:16:29 +0500

    Update Category.js

--- routes/Category.js
+++ routes/Category.js
@@ -5,3 +5,9 @@ const router = express.Router();
 // category controller imports
 const categoryController = require("../controllers/Category");
 const fetchCategories = require("../controllers/Category");
+
+
+/* ==================
+             Products category routes
+             ======================*/
+router.post("/categories", createCategory);
