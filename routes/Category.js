0bf47ebe1013abc210ee1b59f1b81d6993eecde2
Shahbaz Khalid <130798658+ShahbazKhaliddev@users.noreply.github.com>
2024-04-14 13:14:45 +0500

    Update Category.js

--- routes/Category.js
+++ routes/Category.js
@@ -1,3 +1,5 @@
 const express = require("express");
 const router = express.Router();
 
+const categoryController = require("../controllers/Category");
+const fetchCategories = require("../controllers/Category");
