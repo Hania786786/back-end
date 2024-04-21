af905a2b09882171f337a78042ac69df054d8c69
Shahbaz Khalid <130798658+ShahbazKhaliddev@users.noreply.github.com>
2024-04-21 09:25:15 +0500

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 // importing controllers
-
 const fetchCategories = require("../controllers/Category");
 const userController = require("../controllers/User");
 const cartController = require("../controllers/Cart");
